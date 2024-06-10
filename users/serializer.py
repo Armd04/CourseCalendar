17696b91e05ef75ca91ed5d82703347b35d7efc3
Alireza Mohaghegh <mohaghegh.ar82@gmail.com>
2024-06-10 17:21:20 -0400

    Fix register issue

--- users/serializer.py
+++ users/serializer.py
@@ -12,9 +12,9 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'password', 'email']
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=100)
+    password = serializers.CharField(max_length=100)
+    email = serializers.EmailField()
 
 
