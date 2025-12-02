1cf01630d083e30382f048ba64711941bddcfc5f
Alireza Mohaghegh <mohaghegh.ar82@gmail.com>
2025-12-01 22:04:29 -0500

    update default

--- calander-app/src/components/Calendar.tsx
+++ calander-app/src/components/Calendar.tsx
@@ -47,7 +47,7 @@ const firstIndex = (startTime: string, timeSlots: string[]) => {
 }
 
 const Calendar: React.FC = () => {
-  const defaultTerm = { id: '1259', label: 'Fall 2025', value: '1259' };
+  const defaultTerm = { id: '1261', label: 'Winter 2026', value: '1261' };
   const [terms, setTerms] = useState<string[]>([]);
   const [subjects, setSubjects] = useState<string[]>([]);
   const [selectedTerm, setSelectedTerm] = useState<string>();
